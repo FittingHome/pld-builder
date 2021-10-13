f9dfcec0cc4e172c944b038ca3045a7dc8dac30c
Krakor <vincent.faivre@epitech.eu>
2021-10-14 01:44:30 +0200

    ✅ add 2 tests related to pdf-lib

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,23 @@
+
 const {
 	optionalAppend,
 	stringIsUrl,
-} = require('../src/utils');
+	rgbCustom,
+	breakLinesPdf
+} = require('../src/utils')
+
+let pdfDoc;
+let pdfFont
+
+
+beforeAll(() => {
+	const { PDFDocument, StandardFonts } = require('pdf-lib')
+	return new Promise(async resolve => {
+		pdfDoc = await PDFDocument.create()
+		pdfFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+		resolve()
+	});
+});
 
 test('append if not present at the end', () => {
 	expect(optionalAppend('foo', '.jpg')).toEqual('foo.jpg')
@@ -22,4 +38,27 @@ test("check that string is a valid url", () => {
 test("check that string isn't a valid url", () => {
 	expect(stringIsUrl("google.com")).toBeFalsy()
 	expect(stringIsUrl("/www.google.com")).toBeFalsy()
+})
+
+test("rgb is correct with 255 based values", () => {
+	const white = rgbCustom({r: 255, g: 255, b: 255})
+	expect(white.red).toEqual(1)
+	expect(white.green).toEqual(1)
+	expect(white.blue).toEqual(1)
+
+	const black = rgbCustom({r: 0, g: 0, b: 0})
+	expect(black.red).toEqual(0)
+	expect(black.green).toEqual(0)
+	expect(black.blue).toEqual(0)
+})
+
+test("break lines pdf", () => {
+	const paragraphs = breakLinesPdf("The quick brown fox", pdfFont, 20, 100)
+	console.log({paragraphs})
+	expect(paragraphs.length).toBeGreaterThan(1)
+})
+
+test("it should return the same string in an array", () => {
+	const paragraphs = breakLinesPdf("The lazy dog", pdfFont, 20)
+	expect(paragraphs).toEqual(["The lazy dog"])
 })

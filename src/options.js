01e4e67913900d2f850681e7a1ef813663ad1b41
Krakor <vincent.faivre@epitech.eu>
2021-10-13 07:58:59 +0200

    ✨ handle log level verbosity in options 🩹 fix date value in options

--- src/options.js
+++ src/options.js
@@ -1,21 +1,20 @@
-const { Command, Option } = require('commander');
+const { Command, Option } = require('commander')
 
 function parseOptions() {
-	const program = new Command();
+	const program = new Command()
 
-	const now = new Date();
+	const now = new Date()
 
 	program
 		.addOption(new Option('-i, --input <file.json>', 'json with enough information to build the pld'))
 		.addOption(new Option('-v, --version <version>', 'which version of the PLD is it').choices(['KO', 'FU', 'D']).default('KO'))
-		.addOption(new Option('-d, --date <YYYY/MM>', 'what date is it').default(`${now.getFullYear()}/${now.getMonth().toString().padStart(2, "0")}`, 'current date'))
+		.addOption(new Option('-d, --date <YYYY/MM>', 'what date is it').default(`${now.getFullYear()}/${(now.getMonth() + 1).toString().padStart(2, "0")}`, 'current date'))
+		.addOption(new Option('--verbose <level>', 'verbose log level to filter from').choices(['fatal', 'error', 'warn', 'info', 'debug', 'trace']).default('info'))
 
-	program.parse(process.argv);
-	return program.opts();
+	program.parse(process.argv)
+	return program.opts()
 }
 
-const options = parseOptions();
+const options = parseOptions()
 
-module.exports = {
-	options
-}
+module.exports = options

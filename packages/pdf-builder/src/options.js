142afe158762c63c75acc7b75df564316edee60d
Krakor <vincent.faivre@epitech.eu>
2021-10-19 09:20:56 +0200

    🐛 [🏗️] accept unkown options

--- packages/pdf-builder/src/options.js
+++ packages/pdf-builder/src/options.js
@@ -6,6 +6,7 @@ function parseOptions() {
 	const now = new Date()
 
 	program
+		.allowUnknownOption()
 		.addOption(new Option('-i, --input <file.json>', 'json with enough information to build the pld'))
 		.addOption(new Option('-v, --version <version>', 'which version of the PLD is it').choices(['KO', 'FU', 'D']).default('KO'))
 		.addOption(new Option('-d, --date <YYYY/MM>', 'what date is it').default(`${now.getFullYear()}/${(now.getMonth() + 1).toString().padStart(2, "0")}`, 'current date'))
